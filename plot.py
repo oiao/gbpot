0b71c895e36de158416938854471801d5cdb80e0
Leo Komissarov <komissarov@scm.com>
2020-08-05 17:49:56 +0200

    remove cbar

--- plot.py
+++ plot.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from gbpot import gbpot
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-range = (-3, 3, .01)
+range = (-2.5, 2.5, .01)
 u1 = np.array([1,0,0])
 u2 = np.array([1,0,0])
 xx, yy, zz = np.meshgrid(np.arange(*range),np.arange(*range),[0])
@@ -20,19 +19,16 @@ params = (
     (.5, .5, 1., .1),
     )
 
-fig, ax = plt.subplots(4,2, figsize=(9, 14))
+fig, ax = plt.subplots(4,2, figsize=(9.5, 20))
 ax = ax.flatten()
 for i,p in enumerate(params):
     U       = gbpot(P,u1,u2, *p)
-    U[U>0]  = np.nan
+    U[U>0]  = 0
 
     im      = ax[i].contour(xx[:,:,0], yy[:,:,0], U.reshape(len(xx),len(xx)), levels=10)
     ax[i].text(0.01, 0.94, f"$\kappa={p[0]}, \kappa'={p[1]}, \mu={p[2]}, \\nu={p[3]}$", transform=ax[i].transAxes)
+    ax[i].text(0.01, 0.88,  f"Umin = {U.min():.2e}", transform=ax[i].transAxes)
     ax[i].set_aspect('equal', 'box')
-
-    divider = make_axes_locatable(ax[i])
-    cax     = divider.append_axes('right', size='5%', pad=0.05)
-    fig.colorbar(im, format='%.2e', cax=cax)
     ax[i].set_xticks([]); ax[i].set_yticks([])
 
 plt.tight_layout()
